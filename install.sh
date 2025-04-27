6bcfab2042d3dd1330c95f4f46393f30f5d00a4e
Alexander Gharibian <alexandergharibian@gmail.com>
2025-04-27 14:01:11 -0400

    Adding homebrew install

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+# Check if Homebrew is installed, install if not
+if ! command -v brew &> /dev/null; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+brew install stow antidote
 stow zsh/
 stow git/
 stow vim/
